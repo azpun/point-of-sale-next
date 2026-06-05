54f172b39894ae94c4086babade82c0c01a72548
azpun <azpunari@gmail.com>
2026-06-06 06:59:31 +0700

    feat: add lucide-react dependency and enhance Home page with order management features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Card } from "@/components/Card";
+import { Plus } from "lucide-react";
 
 export default function Home() {
   return (
@@ -9,28 +10,47 @@ export default function Home() {
             Sate Soto Madura Mas Niram, Melania
           </h1>
         </div>
-        <div className="flex flex-col  p-4">
-          <div>
+        <div className="flex flex-col gap-4 p-4">
+          <div className="flex justify-between items-center">
             <h2 className="text-lg md:text-xl font-bold">Daftar Pesanan</h2>
+            <button className="hidden md:flex items-center gap-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Tambah Pesanan
+            </button>
           </div>
-          <div className="w-full">
+          <div className="w-full flex flex-col gap-4">
             <Card className="max-w-131.25">
               <div className="p-4">
                 <p>#0001</p>
                 <h3 className="font-bold">Nama-Customer</h3>
                 <p>Daftar Pesanan</p>
                 <p>Total</p>
+                <p>Status</p>
               </div>
               <div className="border-t bg-gray-100">
-                <div className="flex justify-end p-4">
-                  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                <div className="flex justify-end gap-2 p-4">
+                  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex-1">
+                    Lihat
+                  </button>
+                  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex-1">
                     Bayar
+                  </button>
+                  <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                    Batal
                   </button>
                 </div>
               </div>
             </Card>
           </div>
         </div>
+      </div>
+
+      {/* Floating Button Add Order */}
+      <div className="fixed bottom-4 right-4 md:hidden">
+        <button className="size-15 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+          <div className="flex justify-center items-center">
+            <Plus />
+          </div>
+        </button>
       </div>
     </div>
   );
