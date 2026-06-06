00f347ffe359b8019823d637742340dd6ef327fb
azpun <azpunari@gmail.com>
2026-06-06 08:06:30 +0700

    feat: update layout with header and modify Home page for navigation to menus page; add Menu component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
 import { Card } from "@/components/Card";
 import { Plus } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <div className="flex flex-col">
-        <div className="border-b flex justify-center p-4">
-          <h1 className="text-xl font-bold md:text-2xl">
-            Sate Soto Madura Mas Niram, Melania
-          </h1>
-        </div>
         <div className="flex flex-col gap-4 p-4">
           <div className="flex justify-between items-center">
             <h2 className="text-lg md:text-xl font-bold">Daftar Pesanan</h2>
@@ -46,11 +42,13 @@ export default function Home() {
 
       {/* Floating Button Add Order */}
       <div className="fixed bottom-4 right-4 md:hidden">
-        <button className="size-15 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-          <div className="flex justify-center items-center">
-            <Plus />
-          </div>
-        </button>
+        <Link href="/menus">
+          <button className="size-15 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+            <div className="flex justify-center items-center">
+              <Plus />
+            </div>
+          </button>
+        </Link>
       </div>
     </div>
   );
