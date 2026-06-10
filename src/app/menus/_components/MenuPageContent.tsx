d85f631bc50fb526861070f704064caf4f2c6542
azpun <azpunari@gmail.com>
2026-06-10 10:55:00 +0700

    feat: refactor MenuPageContent to improve layout and enhance menu item display with additional details

--- src/app/menus/_components/MenuPageContent.tsx
+++ src/app/menus/_components/MenuPageContent.tsx
@@ -49,73 +49,79 @@ const MenuPageContent = ({ dataMenu }: MenuPageContentProps) => {
           </div>
         </div>
       </div>
-      <div>
-        {/* Cakupan Awal */}
-        {/* nanti akan data akan disajikan dengan bentuk array of object dari menu menggunakan array method map*/}
-        {dataMenu.map(menu => (
-          <Card key={menu.id} className="mx-4">
-            <div className="m-4">
-              <div>
-                <Image
-                  src={menu.imageUrl}
-                  width={700}
-                  height={100}
-                  alt="logo"
-                />
-              </div>
-              <div className="flex justify-between items-center">
-                <h2 className="font-bold text-lg">{menu.name}</h2>
+
+      <div className="space-y-4 flex flex-col md:flex-row w-full ">
+        {/* Daftar Menu */}
+        <div className="space-y-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:w-330">
+          {dataMenu.map(menu => (
+            <Card key={menu.id} className="mx-4">
+              <div className="m-4 space-y-4">
                 <div>
-                  <p>Rp{menu.price.toLocaleString("id-ID")}</p>
-                  <p>Stok: {menu.stock}</p>
+                  <Image
+                    src={menu.imageUrl}
+                    width={700}
+                    height={100}
+                    alt="logo"
+                    loading="eager"
+                    className="w-full h-56 md:h-64 object-cover rounded-md"
+                  />
+                </div>
+                <div className="flex justify-between items-center">
+                  <h2 className="font-bold text-lg">{menu.name}</h2>
+                  <div>
+                    <p>Rp{menu.price.toLocaleString("id-ID")}</p>
+                    <p>Stok: {menu.stock}</p>
+                  </div>
+                </div>
+                <div className="my-4">
+                  <button className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    Tambah ke Keranjang
+                  </button>
                 </div>
               </div>
-              <div className="my-4">
-                <button className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                  Tambah ke Keranjang
-                </button>
+            </Card>
+          ))}
+        </div>
+
+        {/* Detail Pemesan */}
+        <div className="space-y-4 w-150">
+          <div className="">
+            <Card className="mx-4">
+              <div className="m-4 space-y-3">
+                <div>
+                  <h2 className="font-bold text-lg">Detail Pemesan</h2>
+                </div>
+                <div>
+                  <label htmlFor="nama">Nama Pelanggan</label>
+                  <input
+                    type="text"
+                    name="nama"
+                    id="nama"
+                    placeholder="Masukan Nama"
+                    className="w-full border border-gray-600 rounded p-2"
+                  />
+                </div>
+                <div>
+                  <button className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    Simpan Pesanan
+                  </button>
+                </div>
               </div>
-            </div>
-          </Card>
-        ))}
-        {/* Cakupan Akhir */}
-      </div>
-      {/* Detail Pemesan */}
-      <div>
-        <Card className="mx-4">
-          <div className="m-4 space-y-3">
-            <div>
-              <h2 className="font-bold text-lg">Detail Pemesan</h2>
-            </div>
-            <div>
-              <label htmlFor="nama">Nama Pelanggan</label>
-              <input
-                type="text"
-                name="nama"
-                id="nama"
-                placeholder="Masukan Nama"
-                className="w-full border border-gray-600 rounded p-2"
-              />
-            </div>
-            <div>
-              <button className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                Simpan Pesanan
-              </button>
-            </div>
+            </Card>
           </div>
-        </Card>
-      </div>
-      <div>
-        <Card className="mx-4">
-          <div className="m-4 space-y-3">
-            <div>
-              <h2 className="font-bold text-lg">Keranjang Pesanan</h2>
-            </div>
-            <div className="flex flex-col gap-4 bg-gray-300 p-4">
-              <p>Belum ada pesanan</p>
-            </div>
+          <div>
+            <Card className="mx-4">
+              <div className="m-4 space-y-3">
+                <div>
+                  <h2 className="font-bold text-lg">Keranjang Pesanan</h2>
+                </div>
+                <div className="flex flex-col gap-4 bg-gray-300 p-4">
+                  <p>Belum ada pesanan</p>
+                </div>
+              </div>
+            </Card>
           </div>
-        </Card>
+        </div>
       </div>
     </div>
   );
