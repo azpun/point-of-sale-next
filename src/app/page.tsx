a632f54f97560a324e52d7bb94a1b4a4780916aa
azpun <azpunari@gmail.com>
2026-06-11 17:27:40 +0700

    feat: restructure order display with enhanced mobile and desktop layouts, including order details and action buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,11 +9,17 @@ export default function Home() {
         <div className="flex flex-col gap-4 p-4">
           <div className="flex justify-between items-center">
             <h2 className="text-lg md:text-xl font-bold">Daftar Pesanan</h2>
-            <button className="hidden md:flex items-center gap-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-              Tambah Pesanan
-            </button>
+            <div>
+              <Link href="/menus">
+                <button className="hidden md:flex items-center gap-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                  Tambah Pesanan
+                </button>
+              </Link>
+            </div>
           </div>
-          <div className="w-full flex flex-col gap-4">
+
+          {/* mobile version */}
+          <div className="w-full flex flex-col gap-4 md:hidden">
             <Card className="max-w-131.25">
               <div className="p-4">
                 <p>#0001</p>
@@ -35,6 +41,40 @@ export default function Home() {
                 </div>
               </div>
             </Card>
+          </div>
+
+          {/* desktop version */}
+          <div className="hidden m-6 md:flex md:justify-center md:items-center ">
+            <table className="border border-spacing-2 w-full">
+              <thead>
+                <tr>
+                  <th className="w-62 border">Nomor Pesanan</th>
+                  <th className="w-100 border">Nama Pemesan</th>
+                  <th className="w-50 border">Total Pesanan</th>
+                  <th className="w-50 border">Tanggal</th>
+                  <th className="w-50 border">Status</th>
+                  <th className="border">Tombol Aksi</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td className="border p-2">ORD-001</td>
+                  <td className="border p-2">Budi Pekerti</td>
+                  <td className="border p-2">Rp125.000</td>
+                  <td className="border p-2">11-06-2026</td>
+                  <td className="border p-2">
+                    <span className="bg-blue-500 text-white p-2 rounded-lg">
+                      Dalam Antrian
+                    </span>
+                  </td>
+                  <td className="border p-2">
+                    <button className="bg-blue-500 hover:bg-blue-700 p-2 m-2 rounded-md text-white">
+                      Lihat
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
